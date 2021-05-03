c1f4b212a83ae4e8b22174f31fba37713488e5c3
amila96 <64005937+amila96@users.noreply.github.com>
2021-05-03 09:56:10 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DashBoardComponent} from './component/dash-board/dash-board.component';
+import {LogInFormComponent} from './component/log-in-form/log-in-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LogInFormComponent },
+  {path:'DashBord', component:DashBoardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { DashBoardComponent } from './component/dash-board/dash-board.component';
+import { LogInFormComponent } from './component/log-in-form/log-in-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashBoardComponent,
+    LogInFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
