bc1c5661ad1d32e9604abd2de169583427664a9d
amila96 <64005937+amila96@users.noreply.github.com>
2021-05-07 11:35:38 +0530

    bookings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DashBoardComponent} from './component/dash-board/dash-board.component';
 import {LogInFormComponent} from './component/log-in-form/log-in-form.component';
+import {DefaultPageComponent} from './component/dash-board/pages/default-page/default-page.component';
+import {RegisterHotelPageComponent} from './component/dash-board/pages/register-hotel-page/register-hotel-page.component';
 
 const routes: Routes = [
   {path:'', component:LogInFormComponent },
-  {path:'DashBord', component:DashBoardComponent}
+  {path:'DashBord', component:DashBoardComponent, children:[
+    {path: '',component:DefaultPageComponent},
+    {path: 'registerHotel',component:RegisterHotelPageComponent}
+  ]}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,32 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { DashBoardComponent } from './component/dash-board/dash-board.component';
 import { LogInFormComponent } from './component/log-in-form/log-in-form.component';
+import { HeaderComponent } from './component/header/header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { FooterComponent } from './component/footer/footer.component';
+import { DefaultPageComponent } from './component/dash-board/pages/default-page/default-page.component';
+import { RegisterHotelPageComponent } from './component/dash-board/pages/register-hotel-page/register-hotel-page.component';
+import { CardsComponent } from './component/dash-board/pages/default-page/items/cards/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashBoardComponent,
-    LogInFormComponent
+    LogInFormComponent,
+    HeaderComponent,
+    FooterComponent,
+    DefaultPageComponent,
+    RegisterHotelPageComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule
+    FormsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
